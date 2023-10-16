2c4f891edc65c55418c3ea298ad97e44ec99d6d5
alexandre.martel.etu <alexandre.martel.etu@univ-lille.fr>
2023-10-16 15:32:49 +0200

    add UML init

--- src/Model/Hunter.java
+++ src/Model/Hunter.java
@@ -1,5 +1,28 @@
 package Model;
 
-public class Hunter {
+import fr.univlille.iutinfo.cam.player.hunter.IHunterStrategy;
+import fr.univlille.iutinfo.cam.player.perception.ICellEvent;
+import fr.univlille.iutinfo.cam.player.perception.ICoordinate;
+import utils.Subject;
+
+public class Hunter extends Subject implements IHunterStrategy {
+
+    @Override
+    public ICoordinate play() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'play'");
+    }
+
+    @Override
+    public void update(ICellEvent arg0) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'update'");
+    }
+
+    @Override
+    public void initialize(int arg0, int arg1) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'initialize'");
+    }
     
 }
--- src/Model/Monster.java
+++ src/Model/Monster.java
@@ -1,5 +1,28 @@
 package Model;
 
-public class Monster {
+import fr.univlille.iutinfo.cam.player.monster.IMonsterStrategy;
+import fr.univlille.iutinfo.cam.player.perception.ICellEvent;
+import fr.univlille.iutinfo.cam.player.perception.ICoordinate;
+import utils.Subject;
+
+public class Monster extends Subject implements IMonsterStrategy{
+
+    @Override
+    public ICoordinate play() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'play'");
+    }
+
+    @Override
+    public void update(ICellEvent arg0) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'update'");
+    }
+
+    @Override
+    public void initialize(boolean[][] arg0) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'initialize'");
+    }
     
 }
