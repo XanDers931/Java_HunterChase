a9869d694a506b5de13cbb356a9ce6e68c838988
paul-saan <paul.milleville@gmail.com>
2023-10-24 11:37:11 +0200

    creerStage Hunter finis

--- src/View/VueHunter.java
+++ src/View/VueHunter.java
@@ -23,7 +23,7 @@ public class VueHunter implements Observer{
         GridPane gridPane = new GridPane();
         for(int i = 0; i < map.getMap().length; i++){
             for(int j = 0; j < map.getMap()[i].length; j++){
-                gridPane.add(new Label(map.getMap()[i][j]), j , i );
+                gridPane.add(new Label(map.getMap()[i][j].toString()), i, j);
             }
         }
         stage.setScene(new GridPaneScene(gridPane));
