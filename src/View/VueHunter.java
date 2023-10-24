0c99d429283898ad228b03d7cd12b71702fddc54
Julien Bouin <juju@MacBook-Air-de-Julien.local>
2023-10-24 11:46:34 +0200

    test

--- src/View/VueHunter.java
+++ src/View/VueHunter.java
@@ -14,20 +14,19 @@ public class VueHunter implements Observer{
     private Hunter hunter;
 
     public void eventHunter(Hunter hunter){
-        //A REMPLIR
+        //A REMPLIR //
     }
 
-    public Stage creerStage(){
+    public void creerStage(){
         Stage stage = new Stage();
         Maps map = hunter.getMap();
         GridPane gridPane = new GridPane();
         for(int i = 0; i < map.getMap().length; i++){
             for(int j = 0; j < map.getMap()[i].length; j++){
-                gridPane.add(new Label(map.getMap()[i][j].toString()), i, j);
+                gridPane.add(new Label(map.getMap()[i][j]), j , i );
             }
+            
         }
-        stage.setScene(new GridPaneScene(gridPane));
-        return stage;
     }
 
     @Override
