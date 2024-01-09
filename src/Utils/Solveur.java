6e42b5fd4433c1a530d53b4f2aeb45a3deaabbf9
paul-saan <paul.milleville@gmail.com>
2024-01-09 15:57:24 +0100

    suppression de code inutile

--- src/Utils/Solveur.java
+++ src/Utils/Solveur.java
@@ -214,20 +214,6 @@ public class Solveur {
                 }
             }
         }
-        
-        for (int i = 0; i < map.length; i++) {
-            for (int j = 0; j < map[i].length; j++) {
-                System.out.print(map[i][j] + " |");
-            }
-            System.out.println();
-        }System.out.println();
-
-        for (int i = 0; i < mapBrouillard.length; i++) {
-            for (int j = 0; j < mapBrouillard[i].length; j++) {
-                System.out.print(mapBrouillard[i][j] + " |");
-            }
-            System.out.println();
-        }System.out.println();
     }
     
 }
