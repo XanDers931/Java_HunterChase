ce0d95d1c867310b4bb6213abdce278038e2926d
julien.bouin.etu <julien.bouin.etu@univ-lille.fr>
2024-01-11 17:38:20 +0100

    fix mini bug

--- src/Controller/ControlMonsterBot.java
+++ src/Controller/ControlMonsterBot.java
@@ -72,7 +72,7 @@ public class ControlMonsterBot implements ControlMonster {
     private void handleMonsterMovement(MonsterStrategy strategy) {
         Monster monster = view.getMonster();
         if (monster.getGameModel().currentPlayer == 1) {
-            ICoordinate coord = view.isFogOfWar() ? strategy.playBrouilard() : strategy.play();
+            ICoordinate coord = view.isFogOfWar() ? strategy.playBrouillard() : strategy.play();
             int clickedRow = coord.getRow();
             int clickedCol = coord.getCol();
             this.clickedCase = new Coordinate(clickedRow, clickedCol);
